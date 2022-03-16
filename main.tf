6d441d23f20dcbf9cb32985bd3fa1e383600283b
pswitala <p.switala@pslab.eu>
2022-03-16 08:44:13 +0100

    Added path var

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 //Rendering vars for ansible
 data  "template_file" "ansible-tpl" {
-  template = file("../ansible/templates/vars.tpl")
+  template = file("${var.pscloud_ansible_path}ansible/templates/vars.tpl")
   vars = {
     efs_mount_target = var.pscloud_efs_mount_target
     rds_endpoint = var.pscloud_rds_endpoint
@@ -12,5 +12,5 @@ data  "template_file" "ansible-tpl" {
 
 resource "local_file" "ansible-file-yml" {
   content  = data.template_file.ansible-tpl.rendered
-  filename = "../ansible/group_vars/terraform_vars_${var.pscloud_project}.yml"
+  filename = "${var.pscloud_ansible_path}ansible/group_vars/terraform_vars_${var.pscloud_project}.yml"
 }
--- vars.tf
+++ vars.tf
@@ -4,3 +4,5 @@ variable "pscloud_efs_mount_target" {}
 variable "pscloud_rds_endpoint" {}
 variable "pscloud_rds_dbuser" {}
 variable "pscloud_rds_dbpass" {}
+
+variable "pscloud_ansible_path"  { default = "../" }
